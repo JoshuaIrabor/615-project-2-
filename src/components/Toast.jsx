b2c4d318a655f5c4a26f54afe01af576cf8e3de2
Joshua Irabor <jirabor.09@gmail.com>
2026-05-14 10:53:16 -0400

    second commit

--- src/components/Toast.jsx
+++
@@ -1,9 +0,0 @@
-import './Toast.css';
-
-export default function Toast({ msg, success, visible }) {
-  return (
-    <div className={`toast ${visible ? 'show' : ''} ${success ? 'success' : ''}`}>
-      {msg}
-    </div>
-  );
-}
